be04c35a4aaa35cdc1788310b3389cf9a6534cf0
Daniel-Ze <d.zendler@gmx.de>
2022-09-01 08:52:35 +0200

    Update add_refgene_names.py

--- add_refgene_names.py
+++ add_refgene_names.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+
 import pandas as pd
 import sys
 import getopt
@@ -13,7 +14,10 @@ def eprint(*args, **kwargs):
 
 
 def usage():
-	print("concat_gene_names.py -i bedtools_intersect.txt (processed)")
+	'''
+	Print a simple usage info
+	'''
+	print("concat_gene_names.py -i bedtools_intersect.txt (processed) -g EVM_PASA.clean.gff3")
 	sys.exit()
 
 
@@ -42,9 +46,10 @@ def return_dict(line, line_index):
 	gff3_line_dict["Strand"] = line[6]
 	gff3_line_dict["Phase"] = line[7]
 
-    # Split columne 9 and create keys
+    # Split column 9 and create keys
 	col_9 = line[8].split(";")
 	f = 0
+
 	for f in range(len(col_9)):
 		if col_9[f] != '':
 			col_9_dict[col_9[f].split("=")[0]] = col_9[f].split("=")[1]
@@ -55,6 +60,11 @@ def return_dict(line, line_index):
 
 
 def input_data():
+	'''
+	Function to check and parse the input data:
+	  - Bedtools intersect will be loaded as pandas DataFrame
+	  - GFF3 file will be loaded as a nested dictonary
+	'''
 	# If command line input is of length 1 quit
 	if len(sys.argv) == 1:
 		usage()
@@ -87,7 +97,7 @@ def input_data():
 		file_path = "./"
 	
 	# Read in the formatted bedtools intersect file with pandas read_csv()
-	print("[info]\tReading in the bedtools intersect {}".format(input_file))
+	print("[info]\tReading {}".format(input_file))
 	df = pd.read_csv(input_file, delimiter="\t", header=None)
 	
 	# Get the basename of the gff file and add the final output extension
@@ -98,7 +108,7 @@ def input_data():
 		gff_file = [line.rstrip().split("\t") for line in file]
 	
 	# Create a dictonary of dictonaries from the read in gff3 file for faster parsing
-	print("[info]\tPreparing dictonary of {}".format(gff_input_file))
+	print("[info]\tCreate GFF3 dictonary {}".format(gff_input_file))
 	gff_dict = {}
 	count = 0
 
@@ -118,9 +128,11 @@ def input_data():
 	#							   ... (column 9 is completely expanded) }
 	#			 ... (continue for every line in gff3 file)
 	# 			}
+	gff_file_length = len(gff_file)
 	for f in gff_file:
 		gff_dict[return_dict(f, count)["ID"]]=return_dict(f, count)
 		count = count + 1
+		print("[info]\t{} %".format(round(count / gff_file_length * 100, 2)), end="\r", flush=True)
 
 	return df, gff_dict, out_file, file_path
 
@@ -133,26 +145,35 @@ def main():
 	output = open(out_file, 'w')
 
 	# Group the one to many reference gene annotation
-	print("[info]\tGrouping one to many reference gene annotations.")
 	df[3] = df.groupby([0])[1].transform(lambda x: ','.join(x))
 
+	# Remove column 1 and remove duplicates
 	df = df.drop(columns=1)
 	df = df.drop_duplicates()
 
+	# Get gene ID from new annotation
 	df[["Type","ID"]] = df[0].str.split(';', expand=True)[0].str.split('=', expand=True)
 
+	# Remove Type column
 	df = df.drop(columns="Type")
+
+	# Count ',' as evidence for multiple reference gene IDs
 	df[4] = df[3].str.count(",")
 	df[4] = df[4] + 1
 	df.sort_values(by=[4], ascending=False)
+
+	# Give the columns recognizable names
 	df.columns = ["Col_9", "ref_gene_ID", "anno_gene_ID", "count_ref_gene_ID"]
 
+	# Write results to file (tab delimited)
 	df.to_csv(file_path+"/gff3_intersect_1tomany_sort.tsv", sep='\t',index=False, columns=["anno_gene_ID", "ref_gene_ID", "count_ref_gene_ID"])
 
+	# Summarize the column count_ref_gene_ID
 	one_to_many = pd.DataFrame(data=df["count_ref_gene_ID"].value_counts())
 	one_to_many = one_to_many.reset_index()
 	one_to_many.columns = ["one_to_many", "counts"]
 	
+	# Write to file for Rscript
 	one_to_many.to_csv(file_path+"/1tomany_overview.tsv", sep="\t", index=False)
 	
 	print("[info]\tAnnoted genes with reference gene ID: {}\n".format(len(df.index)))
@@ -160,11 +181,21 @@ def main():
 	print("\n")
 
 	# Add the reference annotation gene IDs to the last column of the new 
-	# annotation with the flag Note; Adjust this if you want another flag 
+	# annotation with the flag ref_geneID; Adjust this if you want another flag 
 	# column 9 for that
-	print("[info]\tIterate over grouped intersect list and add them to the gff dictonary.")
+	# Multiple runs with different reference annotations will update the field
+	# ref_geneID. The additional reference gene ID will be added to the key as 
+	# a list of values
+	#print("[info]\tIterate over grouped intersect list and add them to the gff dictonary.")
 	for index, row in df.iterrows():
-		gff_dict[row["anno_gene_ID"]]["Note"]=row["ref_gene_ID"]
+		# Check if ref_geneID already exists as key and append if so
+		if "ref_geneID" in gff_dict[row["anno_gene_ID"]]:
+			gff_dict[row["anno_gene_ID"]]["ref_geneID"] = [gff_dict[row["anno_gene_ID"]]["ref_geneID"], row["ref_gene_ID"]]
+		
+		# If not generate key and add reference gene ID
+		else:
+			gff_dict[row["anno_gene_ID"]]["ref_geneID"]=row["ref_gene_ID"]
+		
 	
 	# Format the dictonary content to propper gff3 format and write to file
 	# This works only if the 
@@ -174,6 +205,7 @@ def main():
 		col_18 = ""
 		for key in f_info:
 			if key == "Index":
+				# Index not needed
 				pass
 			if key == "Sequence":
 				col_18 = col_18 + f_info[key]+"\t"
@@ -191,10 +223,13 @@ def main():
 				col_18 = col_18 + f_info[key]+"\t"
 			if key == "Phase":
 				col_18 = col_18 + f_info[key]+"\t"
-			if key == "ID" or key == "Name" or key == "Parent" or key == "Note":
-				col_9 = col_9 + str(key+"="+f_info[key]+";")
+			if key == "ID" or key == "Name" or key == "Parent" or key == "ref_geneID":
+				if isinstance(f_info[key], str):
+					col_9 = col_9 + str(key + "=" + f_info[key] + ";")
+				else:
+					col_9 = col_9 + str(key + "=" + ','.join(f_info[key]) + ";")
 		
-		output.write(col_18+"\t"+col_9+"\n")
+		output.write(col_18+col_9+"\n")
 	output.close()
 
 
